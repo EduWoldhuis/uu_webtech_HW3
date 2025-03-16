50294ac9da65b6ef6365cb2e3f077fc1af74c16a
EduWoldhuis <woldhuis.edu@gmail.com>
2025-03-16 22:05:29 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,34 @@
+var http = require('http');
+var fs = require('fs');
+
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('./data/database.db');
+
+
+
+http.createServer(function (request, response) {
+  request.on('error', function(err) {
+    console.error(err);
+    response.statusCode = 400;
+    response.end();
+  });
+  response.on('error', function(err){
+    console.error(err);
+  });
+  if (request.method === "POST" && request.url === '/register'){
+    request.pipe(response)
+  }
+  else if (request.method === "GET" && request.url === "/index") {
+    fs.readFile("test.html", function(err, data){
+      response.writeHead(200, {'Content-Type': 'text/html'});
+      response.write(data);
+      return response.end();
+    });
+  }
+  else {
+    response.writeHead(404, {'Content-Type': 'text/plain'});
+    response.end("Not found");
+  }
+  //response.writeHead(200, {'Content-Type': 'text/html'});
+  //response.end('Hello World!');
+}).listen(8080, '127.0.0.1'); 
---
+++ database.js
@@ -0,0 +1,8 @@
+var fs = require("fs");
+var file = "test.db";
+var exists = fs.existsSync(file);
+
+var sqlite3 = require("sqlite3").verbose();
+var db = new sqlite3.Database("./test.db")
+
+
