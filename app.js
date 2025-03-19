d1d40c5b892dc2f84828b514c43570a72f431ca4
EduWoldhuis <woldhuis.edu@gmail.com>
2025-03-19 09:40:20 +0100

    actually getting stuff set up

--- app.js
+++ app.js
@@ -1,34 +1,26 @@
 var http = require('http');
 var fs = require('fs');
-
+var express = require("express");
+var bodyParser = require("body-parser");
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('./data/database.db');
 
+const app = express();
 
+app.use(
+  bodyParser.urlencoded({extended: true})
+);
 
-http.createServer(function (request, response) {
-  request.on('error', function(err) {
-    console.error(err);
-    response.statusCode = 400;
-    response.end();
-  });
-  response.on('error', function(err){
-    console.error(err);
-  });
-  if (request.method === "POST" && request.url === '/register'){
-    request.pipe(response)
+app.get("/",function (req, res) {
+  res.sendFile("test.html");
+});
+
+app.post("/api/register", {
+  function (req, res) {
+    let username = req.body.username;
+    let password = req.body.password;
+    res.send("received:" + username + password);
   }
-  else if (request.method === "GET" && request.url === "/index") {
-    fs.readFile("test.html", function(err, data){
-      response.writeHead(200, {'Content-Type': 'text/html'});
-      response.write(data);
-      return response.end();
-    });
-  }
-  else {
-    response.writeHead(404, {'Content-Type': 'text/plain'});
-    response.end("Not found");
-  }
-  //response.writeHead(200, {'Content-Type': 'text/html'});
-  //response.end('Hello World!');
-}).listen(8080, '127.0.0.1'); 
+})
+
+app.listen(8080);
--- database.js
+++ database.js
@@ -5,4 +5,24 @@ var exists = fs.existsSync(file);
 var sqlite3 = require("sqlite3").verbose();
 var db = new sqlite3.Database("./test.db")
 
+db.serialize(() => {
+  db.run(`
+          CREATE TABLE IF NOT EXISTS User (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          username TEXT UNIQUE NOT NULL,
+          password TEXT NOT NULL,
+          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+    `, (err) => {if (err) {console.error('Error creating table User.')}});
 
+});
+
+function createUser(username, password) {
+  const insertQuery = db.prepare("INSERT INTO User VALUES (?, ?)");
+  insertQuery.run([username, password], (err) => {if (err) {console.error("error inserting");}});
+  insertQuery.finalize();
+}
+
+function closeDB() {
+  db.close();
+}
